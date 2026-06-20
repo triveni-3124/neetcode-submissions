31a99783a0d47ae890eca4b5e64896c9cdcbc435
T N <triveninahak03@gmail.com>
2026-06-20 18:00:30 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            int key = nums[i];
+            int j=i-1;
+            while(j>=0 && nums[j]>key){
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }
+    }
+}
